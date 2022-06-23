636a8665e45a5e1b75cf003ee3c197e838d4c418
miezuit <miezuit@gmail.com>
2022-06-23 18:48:22 +0300

    vuecrud

--- vuecrud/frontend/src/router.js
+++ vuecrud/frontend/src/router.js
@@ -6,22 +6,22 @@ const routes =  [
     name: "users",  //optional name to use when we link to this route.
     component: () => import("./components/Home") //component to load when this route is called.
   },
-//   {
-//     path: "/users/:id",
-//     name: "user-details",
-//     component: () => import("./components/ProfilUser")
-//   },
+  {
+    path: "/users/:id",
+    name: "user-details",
+    component: () => import("./components/ProfilUser")
+  },
 //   {
 //     path: "/admin",  //the URL path where this route can be found.
 //     alias: "/admin",
 //     name: "admin",  //optional name to use when we link to this route.
 //     component: () => import("./components/Admin") //component to load when this route is called.
 //   },
-//   {
-//     path: "/add",
-//     name: "add",
-//     component: () => import("./components/AddUser")
-//   },
+  {
+    path: "/add",
+    name: "add",
+    component: () => import("./components/AddUser")
+  },
   //{ path: '*', component: () => import("./components/NotFound")}
   //* se potriveste cu toate rutele (e precum default din switch, sau ultimul else)
 ];
