b7a19d0dd0ad9b24ee1ceb4a43641364d750ead1
miezuit <miezuit@gmail.com>
2022-06-23 17:30:19 +0300

    vuecrud

---
+++ angularcrud/frontend/src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { User } from '../models/user';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+users:User[] = [];
+  constructor(private apiService:ApiService) { }
+
+  ngOnInit(): void {
+    this.readUsers();
+  }
+  readUsers() {
+    // read
+    this.apiService.readUsers().subscribe((users: User[]) => {
+      this.users = users['data'];
+     
+    });
+  }
+
+}
---
+++ curs3/vue/ex2.js
@@ -0,0 +1,30 @@
+
+var vm = new Vue({
+    el: '#app',
+    data: {
+        fname: "Ion",
+        lname: "Popescu",
+        address: "Brasov",
+        message: "Imi place JavaScript"
+    },
+    methods: {
+        myName: function () {
+            return "Eu ma numesc " + this.fname + " " + this.lname + " si sunt din " + this.address;
+        }
+    }
+});
+var app = new Vue({
+    el: '#app2',
+    data: {
+        seen: false,
+        limbaje: [
+            { text: 'JavaScript' },
+            { text: 'Java' },
+            { text: 'PHP' }
+        ],
+        htmlcontent : `<div><h3>Vue 
+        Js este 
+        interesant!</h3></div>`
+    },
+
+})
